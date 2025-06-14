745ebeb917306c2202a3037d40c16e7fbbdeb58c
tndev24 <129576125+tndev24@users.noreply.github.com>
2025-06-14 12:30:08 +0700

    update backend ip

--- client/src/config/request.jsx
+++ client/src/config/request.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import cookies from 'js-cookie';
 
 const request = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://3.231.216.193:3000',
     withCredentials: true,
 });
 
